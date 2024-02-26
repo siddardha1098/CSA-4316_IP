48e02d901e49d8a02f3b8a61c81125fb7341f7b8
siddardha1098 <112462514+siddardha1098@users.noreply.github.com>
2024-02-26 09:52:49 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$db_name = "mysql:host=localhost;dbname=acetheor_grocery ";
+$username = "acewinter27";
+$password = "Manik@20031627";
+
+$conn = new PDO($db_name, $username, $password);
+
+?>
